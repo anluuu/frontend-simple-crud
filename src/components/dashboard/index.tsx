dfdb5dd78fa491883c3dfbd90e862ebc064a2095
Luan <luancunha12@gmail.com>
2020-12-08 17:45:41 -0300

    dashboard

--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -1,11 +1,56 @@
 import React from 'react';
+import {
+  Card,
+  Elevation,
+  InputGroup,
+  FormGroup,
+  Button,
+} from '@blueprintjs/core';
+import {
+  Container,
+  CardTitle,
+  Form,
+  FormContainer,
+  BottomContent,
+} from './styles';
+import { useAuth } from '../../hooks/Auth';
 
 const Dashboard = (): React.ReactElement => {
+  const { signOut, user } = useAuth();
   return (
-    <h1>
-      hello world
-    </h1>
-  )
+    <Container>
+      <Card elevation={Elevation.FOUR}>
+        <CardTitle>
+          <h4>Dashboard</h4>
+        </CardTitle>
+        <Form>
+          <FormContainer>
+            <FormGroup label="Logged User e-mail" labelFor="email">
+              <InputGroup
+                id="email"
+                leftIcon="envelope"
+                // @ts-ignore
+                value={user.email}
+                disabled={!!user}
+              />
+            </FormGroup>
+            <FormGroup label="Logged user name" labelFor="password">
+              <InputGroup
+                id="name"
+                leftIcon="user"
+                value={user.name}
+                // @ts-ignore
+                disabled={!!user}
+              />
+            </FormGroup>
+          </FormContainer>
+        </Form>
+        <BottomContent>
+          <Button text="Logout" onClick={signOut} />
+        </BottomContent>
+      </Card>
+    </Container>
+  );
 };
 
 export default Dashboard;
---
+++ src/components/dashboard/styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin: auto;
+  width: 400px;
+  padding: 100px 0;
+  text-align: center;
+`;
+
+export const CardTitle = styled.div`
+  justify-content: center;
+  display: flex;
+`;
+
+export const Form = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const FormContainer = styled.div`
+  width: 100%;
+  max-width: 300px;
+`;
+
+export const BottomContent = styled.div``;
